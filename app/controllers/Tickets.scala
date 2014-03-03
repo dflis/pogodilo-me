c76927920306a47a76787437c79ee72c2bd3bcc0
Darko Drezga <darko.drezga@gmail.com>
2014-03-03 23:09:52 +0100

    no message

--- app/controllers/Tickets.scala
+++ app/controllers/Tickets.scala
@@ -9,10 +9,12 @@ import model.Tables._
 object Tickets extends Controller {
   def list = DBAction { implicit rs =>
     val query = for {
-      ((((bt, btm),m),c1),c2) <- BetTicket leftJoin BetTicketMatches on (_.id === _.betTicketId) leftJoin Match on (_._2.matchId === _.id) leftJoin Competitor on (_._2.home === _.id) leftJoin Competitor on (_._1._2.visitor === _.id)
-    } yield (bt,m,c1,c2)
+      ((((bt, btm),m),c1),c2) <- (BetTicket leftJoin BetTicketMatches on (_.id === _.betTicketId) 
+      										leftJoin Match on (_._2.matchId === _.id) 
+      										leftJoin Competitor on (_._2.home === _.id) 
+      										leftJoin Competitor on (_._1._2.visitor === _.id))
+    } yield (bt,m,(c1,c2))
     val result = query.list.groupBy(_._1).mapValues(x=>x.groupBy(_._2))
-    //val result = query.list.groupBy(x=>(x._1,x._2))    
-    Ok(result.mkString("\n"))
+    Ok(result.head.toString + "\n\n" + result.mkString("\n\n"))
   }
 }
