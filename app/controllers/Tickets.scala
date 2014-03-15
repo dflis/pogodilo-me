40dbb2ce9b994f51096ebc10e4f5e4a050a7e745
Darko Drezga <darko.drezga@gmail.com>
2014-03-15 19:50:03 +0100

    prilagođen template i controller za listiće

--- app/controllers/Tickets.scala
+++ app/controllers/Tickets.scala
@@ -7,25 +7,20 @@ import play.api.db.slick.Config.driver.simple._
 import model.Tables._
 
 object Tickets extends Controller {
-  def list = DBAction { implicit rs =>
+
+  def list(tId: Int = 1) = DBAction { implicit rs =>
     val query = for {
-      ((((bt, btm),m),c1),c2) <- (BetTicket leftJoin BetTicketMatches on (_.id === _.betTicketId) 
-      										leftJoin Match on (_._2.matchId === _.id) 
-      										leftJoin Competitor on (_._2.home === _.id) 
-      										leftJoin Competitor on (_._1._2.visitor === _.id))
+      ((((bt, btm),m),c1),c2) <- (BetTicket leftJoin BetTicketMatches on (_.id === _.betTicketId)
+        leftJoin Match on (_._2.matchId === _.id)
+        leftJoin Competitor on (_._2.home === _.id)
+        leftJoin Competitor on (_._1._2.visitor === _.id)) if m.tournamentId === tId
     } yield (bt,m,(c1,c2))
 
-    val query2 = for {
-      btm <- BetTicketMatches
-      bt <- btm.betTicketFk
-      m <- btm.matchFk
-      c1 <- m.competitorFk1
-      c2 <- m.competitorFk2
-    } yield (bt,m,(c1,c2))
-    
-    val d = query.list diff query2.list
-    
-    val result = query.list.groupBy(_._1).mapValues(_ map (x => (x._2, x._3))).mapValues(_ groupBy (_._1)).mapValues(_.mapValues(_ map (_._2)))
-    Ok(result.head.toString + "\n\n" + result.mkString("\n\n") + "\n\n" + d.toString)
+    val tournament = Tournament.where(_.id === tId).list().head
+
+    //val result = query.list.groupBy(_._1).mapValues(_ map (x => (x._2, x._3))).mapValues(_ groupBy (_._1)).mapValues(_.mapValues(_ map (_._2)))
+    val result = query.list.groupBy(x=>(x._1,x._2)).mapValues(_ map (_._3)).groupBy(_._1._1)
+    //Ok(tournament.displayName + "\n\n" + result.mkString("\n\n") + "\n\n" + query.selectStatement)
+    Ok(views.html.tickets(result))
   }
 }
