df3940df640b00ed150f9c775957a4bbbbb48d55
Darko Drezga <darko.drezga@gmail.com>
2014-03-22 21:55:05 +0100

    prilagođen template i controller za listiće

--- app/controllers/Tickets.scala
+++ app/controllers/Tickets.scala
@@ -16,11 +16,7 @@ object Tickets extends Controller {
         leftJoin Competitor on (_._1._2.visitor === _.id)) if m.tournamentId === tId
     } yield (bt,m,(c1,c2))
 
-    val tournament = Tournament.where(_.id === tId).list().head
-
-    //val result = query.list.groupBy(_._1).mapValues(_ map (x => (x._2, x._3))).mapValues(_ groupBy (_._1)).mapValues(_.mapValues(_ map (_._2)))
     val result = query.list.groupBy(x=>(x._1,x._2)).mapValues(_ map (_._3)).groupBy(_._1._1)
-    //Ok(tournament.displayName + "\n\n" + result.mkString("\n\n") + "\n\n" + query.selectStatement)
-    Ok(views.html.tickets(result))
+     Ok(views.html.tickets(result))
   }
 }
